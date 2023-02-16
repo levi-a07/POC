072afc69bbfbfeddc6f9b51dc8d012c771cbb9f0
Laxmi Anandache <Laxmi.Anandache@Clarivate.com>
2023-02-16 16:48:35 +0530

    removed syso

--- src/com/org/linting/Main.java
+++ src/com/org/linting/Main.java
@@ -8,7 +8,7 @@ public class Main{
 
     public static void main(String[]args) {
         final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        System.out.println("asdsladlsad");
+    
         logger.log(Level.INFO, "HELOO LINTINg USED HERE");
     }
 }
