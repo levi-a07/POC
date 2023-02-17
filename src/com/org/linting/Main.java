08fdb4a81eda75f27f999dc25bcb1f25aa1c0efe
Laxmi Anandache <Laxmi.Anandache@Clarivate.com>
2023-02-17 14:13:14 +0530

    test

--- src/com/org/linting/Main.java
+++ src/com/org/linting/Main.java
@@ -3,10 +3,12 @@ package com.org.linting;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Main {public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args){
+
 
         final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         logger.log(Level.INFO, "HELLO LINTING USED HERE");
-
-        System.out.println("afdfds");
-    } }
+        logger.log(Level.INFO, "HELLO LINTING USED HERE");
+    }
+}
